5347bdeb6789399001cd97201b8bc320b54e5aae
Kurasao <jamerolenor@gmail.com>
2023-12-19 08:53:08 +0800

    added new column in customers table

--- database/migrations/2023_12_18_100001_create_customers_table.php
+++ database/migrations/2023_12_18_100001_create_customers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id('customer_id');
             $table->string('first_name');
             $table->string('last_name');
+            $table->string('address');
             $table->string('email')->unique();
             $table->string('hashed_password');
             $table->timestamps();
