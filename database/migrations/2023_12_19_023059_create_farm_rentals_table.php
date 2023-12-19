f95046ba6e64aff233530c6cfbd3651f28c4fc68
Kurasao <jamerolenor@gmail.com>
2023-12-19 10:44:29 +0800

    added farm rentals table

---
+++ database/migrations/2023_12_19_023059_create_farm_rentals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('farm_rentals', function (Blueprint $table) {
+          $table->id('rental_id');
+          $table->string('location_block');
+          $table->string('location_lot');
+          $table->string('nectares');
+          $table->decimal('rental_amount', 8, 2);
+          $table->boolean('availability');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('farm_rentals');
+    }
+};
